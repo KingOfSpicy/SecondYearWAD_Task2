2a47859aabc65ff668fa7c990f8dfb8d12e8d3a9
KingOfSpicy <sangowosan@gmail.com>
2022-02-26 13:22:10 +0200

    Commit what works done before.

---
+++ Data/CheWei_WAD_Task_S2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CheWei_WAD_Task_S2.Models;
+
+namespace CheWei_WAD_Task_S2.Data
+{
+    public class CheWei_WAD_Task_S2Context : DbContext
+    {
+        public CheWei_WAD_Task_S2Context (DbContextOptions<CheWei_WAD_Task_S2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CheWei_WAD_Task_S2.Models.Course> Course { get; set; }
+
+        public DbSet<CheWei_WAD_Task_S2.Models.Enrollment> Enrollment { get; set; }
+
+        public DbSet<CheWei_WAD_Task_S2.Models.Student> Student { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CheWei_WAD_Task_S2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
